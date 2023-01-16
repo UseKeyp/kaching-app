bcdd841d7480bfa3516c85cf3c55c792823cffb1
Devian <devianyeth@gmail.com>
2023-01-16 17:19:32 +0530

    clean up scaffold

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+
+    return config;
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
