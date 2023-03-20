6df50a3bd602af6243f7d24d7022a237f7950ec7
Mike Starr <imhs@protonmail.com>
2023-03-19 21:16:59 -0400

    fixed fonts directories

--- components/Fonts.tsx
+++ components/Fonts.tsx
@@ -5,9 +5,9 @@ const Fonts = () => (
     styles={`
     @font-face {
       font-family: "Sharpie-Variable";
-      src: url("../public/fonts/Sharpie-Variable.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Variable.woff") format("woff"),
-        url("../public/fonts/Sharpie-Variable.ttf") format("truetype");
+      src: url("public/fonts/Sharpie-Variable.woff2") format("woff2"),
+        url("public/fonts/Sharpie-Variable.woff") format("woff"),
+        url("public/fonts/Sharpie-Variable.ttf") format("truetype");
       font-weight: 300 900;
       font-display: swap;
       font-style: normal;
@@ -15,9 +15,9 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Light";
-      src: url("../public/fonts/Sharpie-Light.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Light.woff") format("woff"),
-        url("../public/fonts/Sharpie-Light.ttf") format("truetype");
+      src: url("public/fonts/Sharpie-Light.woff2") format("woff2"),
+        url("public/fonts/Sharpie-Light.woff") format("woff"),
+        url("public/fonts/Sharpie-Light.ttf") format("truetype");
       font-weight: 300;
       font-display: swap;
       font-style: normal;
@@ -25,9 +25,9 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Regular";
-      src: url("../public/fonts/Sharpie-Regular.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Regular.woff") format("woff"),
-        url("../public/fonts/Sharpie-Regular.ttf") format("truetype");
+      src: url("public/fonts/Sharpie-Regular.woff2") format("woff2"),
+        url("public/fonts/Sharpie-Regular.woff") format("woff"),
+        url("public/fonts/Sharpie-Regular.ttf") format("truetype");
       font-weight: 400;
       font-display: swap;
       font-style: normal;
@@ -35,9 +35,9 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Bold";
-      src: url("../public/fonts/Sharpie-Bold.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Bold.woff") format("woff"),
-        url("../public/fonts/Sharpie-Bold.ttf") format("truetype");
+      src: url("public/fonts/Sharpie-Bold.woff2") format("woff2"),
+        url("public/fonts/Sharpie-Bold.woff") format("woff"),
+        url("public/fonts/Sharpie-Bold.ttf") format("truetype");
       font-weight: 700;
       font-display: swap;
       font-style: normal;
@@ -45,9 +45,9 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Extrabold";
-      src: url("../public/fonts/Sharpie-Extrabold.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Extrabold.woff") format("woff"),
-        url("../public/fonts/Sharpie-Extrabold.ttf") format("truetype");
+      src: url("public/fonts/Sharpie-Extrabold.woff2") format("woff2"),
+        url("public/fonts/Sharpie-Extrabold.woff") format("woff"),
+        url("public/fonts/Sharpie-Extrabold.ttf") format("truetype");
       font-weight: 800;
       font-display: swap;
       font-style: normal;
@@ -55,9 +55,9 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Black";
-      src: url("../public/fonts/Sharpie-Black.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Black.woff") format("woff"),
-        url("../public/fonts/Sharpie-Black.ttf") format("truetype");
+      src: url("public/fonts/Sharpie-Black.woff2") format("woff2"),
+        url("public/fonts/Sharpie-Black.woff") format("woff"),
+        url("public/fonts/Sharpie-Black.ttf") format("truetype");
       font-weight: 900;
       font-display: swap;
       font-style: normal;
