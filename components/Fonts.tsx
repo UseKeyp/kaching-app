d4d95513ef90ade3a11297f8b176047141bad7b1
Mike Starr <imhs@protonmail.com>
2023-03-20 11:15:04 -0400

    change fonts directory

--- components/Fonts.tsx
+++ components/Fonts.tsx
@@ -5,10 +5,10 @@ const Fonts = () => (
     styles={`
     @font-face {
       font-family: "Sharpie-Variable";
-      src: url("../public/fonts/Sharpie-Variable.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Variable.woff") format("woff"),
-        url("../public/fonts/Sharpie-Variable.ttf") format("truetype");
-        url("../public/fonts/Sharpie-Variable.eot") format("opentype");
+      src: url("../fonts/Sharpie-Variable.woff2") format("woff2"),
+        url("../fonts/Sharpie-Variable.woff") format("woff"),
+        url("../fonts/Sharpie-Variable.ttf") format("truetype");
+        url("../fonts/Sharpie-Variable.eot") format("opentype");
       font-weight: 300 900;
       font-display: swap;
       font-style: normal;
@@ -17,10 +17,10 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Light";
-      src: url("../public/fonts/Sharpie-Light.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Light.woff") format("woff"),
-        url("../public/fonts/Sharpie-Light.ttf") format("truetype");
-        url("../public/fonts/Sharpie-Light.eot") format("opentype");
+      src: url("../fonts/Sharpie-Light.woff2") format("woff2"),
+        url("../fonts/Sharpie-Light.woff") format("woff"),
+        url("../fonts/Sharpie-Light.ttf") format("truetype");
+        url("../fonts/Sharpie-Light.eot") format("opentype");
       font-weight: 300;
       font-display: swap;
       font-style: normal;
@@ -29,10 +29,10 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Regular";
-      src: url("../public/fonts/Sharpie-Regular.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Regular.woff") format("woff"),
-        url("../public/fonts/Sharpie-Regular.ttf") format("truetype");
-        url("../public/fonts/Sharpie-Regular.eot") format("opentype");
+      src: url("../fonts/Sharpie-Regular.woff2") format("woff2"),
+        url("../fonts/Sharpie-Regular.woff") format("woff"),
+        url("../fonts/Sharpie-Regular.ttf") format("truetype");
+        url("../fonts/Sharpie-Regular.eot") format("opentype");
       font-weight: 400;
       font-display: swap;
       font-style: normal;
@@ -41,10 +41,10 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Bold";
-      src: url("../public/fonts/Sharpie-Bold.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Bold.woff") format("woff"),
-        url("../public/fonts/Sharpie-Bold.ttf") format("truetype");
-        url("../public/fonts/Sharpie-Bold.eot") format("opentype");
+      src: url("../fonts/Sharpie-Bold.woff2") format("woff2"),
+        url("../fonts/Sharpie-Bold.woff") format("woff"),
+        url("../fonts/Sharpie-Bold.ttf") format("truetype");
+        url("../fonts/Sharpie-Bold.eot") format("opentype");
       font-weight: 700;
       font-display: swap;
       font-style: normal;
@@ -53,10 +53,10 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Extrabold";
-      src: url("../public/fonts/Sharpie-Extrabold.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Extrabold.woff") format("woff"),
-        url("../public/fonts/Sharpie-Extrabold.ttf") format("truetype");
-        url("../public/fonts/Sharpie-Extrabold.eot") format("opentype");
+      src: url("../fonts/Sharpie-Extrabold.woff2") format("woff2"),
+        url("../fonts/Sharpie-Extrabold.woff") format("woff"),
+        url("../fonts/Sharpie-Extrabold.ttf") format("truetype");
+        url("../fonts/Sharpie-Extrabold.eot") format("opentype");
       font-weight: 800;
       font-display: swap;
       font-style: normal;
@@ -65,10 +65,10 @@ const Fonts = () => (
 
     @font-face {
       font-family: "Sharpie-Black";
-      src: url("../public/fonts/Sharpie-Black.woff2") format("woff2"),
-        url("../public/fonts/Sharpie-Black.woff") format("woff"),
-        url("../public/fonts/Sharpie-Black.ttf") format("truetype");
-        url("../public/fonts/Sharpie-Black.eot") format("opentype");
+      src: url("../fonts/Sharpie-Black.woff2") format("woff2"),
+        url("../fonts/Sharpie-Black.woff") format("woff"),
+        url("../fonts/Sharpie-Black.ttf") format("truetype");
+        url("../fonts/Sharpie-Black.eot") format("opentype");
       font-weight: 900;
       font-display: swap;
       font-style: normal;
