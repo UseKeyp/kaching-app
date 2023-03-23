51cd7c5d091e05301db6549a6f91f3deaaf451ba
Mike Starr <imhs@protonmail.com>
2023-03-23 17:47:21 -0400

    confirmation page height calculations

--- pages/confirmation.tsx
+++ pages/confirmation.tsx
@@ -24,60 +24,64 @@ const Confirmation = () => {
   };
 
   return (
-    <Flex flexDirection="column" justifyContent="space-between" h="80vh">
+    <>
       <Navbar setNavHeight={setNavHeight} />
-      <VStack fontWeight="extrabold" fontSize="3rem">
-        <Box w="full">
-          <Heading
-            w="full"
-            as="h1"
-            fontSize={["6rem", "8rem"]}
-            fontWeight="extrabold"
-          >
-            Kaching
-          </Heading>
-        </Box>
-        <Stack w="full" spacing={"-0.75rem"} pb="3rem">
-          <HStack>
-            <Text color="formGreen">
-              {/* {asset === "USDC" ? "$" : null}
-              {amount} */}
-              5 MATIC
-            </Text>
-            <Text color="assetOrange">{/* {asset} */}</Text>
-          </HStack>
-          <Box>
-            <Text color="loginGray">Sent to</Text>
-          </Box>
-          <Box>
-            <Text color="formBlueDark">
-              {/* {username} */}
-              user@gmail.com
-            </Text>
-          </Box>
-        </Stack>
-        <Stack w="full" spacing={"-0.75rem"}>
-          <Box>
-            <Text color="lightGray">Transaction Sent!</Text>
-          </Box>
-          <Box onClick={handleViewTx}>
-            <Text color="formBlueDark">View</Text>
-          </Box>
-        </Stack>
-      </VStack>
       <Box
-        mt="2rem"
-        mx="-1.5rem"
-        position="relative"
-        bottom={-10}
-        right={0}
-        left={0}
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        h={`calc(100vh - ${navHeight}px)`}
       >
-        <Link href="/">
-          <Button variant="formBlue">Return</Button>
-        </Link>
+        <VStack fontWeight="extrabold" fontSize="3rem">
+          <Box w="full">
+            <Heading
+              w="full"
+              as="h1"
+              fontSize={["6rem", "8rem"]}
+              fontWeight="extrabold"
+            >
+              Kaching
+            </Heading>
+          </Box>
+          <Stack w="full" spacing={"-0.75rem"} pb="3rem">
+            <HStack>
+              <Text color="formGreen">
+                {/* {asset === "USDC" ? "$" : null}
+              {amount} */}
+                5 MATIC
+              </Text>
+              <Text color="assetOrange">{/* {asset} */}</Text>
+            </HStack>
+            <Box>
+              <Text color="loginGray">Sent to</Text>
+            </Box>
+            <Box>
+              <Text color="formBlueDark">
+                {/* {username} */}
+                user@gmail.com
+              </Text>
+            </Box>
+          </Stack>
+          <Stack w="full" spacing={"-0.75rem"}>
+            <Box>
+              <Text color="lightGray">Transaction Sent!</Text>
+            </Box>
+            <Box onClick={handleViewTx}>
+              <Text color="formBlueDark">View</Text>
+            </Box>
+          </Stack>
+        </VStack>
+        <Box
+          mt="1rem"
+          mx="-1.5rem"
+          // mb="-0.5rem"
+        >
+          <Link href="/">
+            <Button variant="formBlue">Return</Button>
+          </Link>
+        </Box>
       </Box>
-    </Flex>
+    </>
   );
 };
 
