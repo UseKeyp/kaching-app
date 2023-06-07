74fcf0e5664eff6c892203508bb313c71ced3a44
alekseevaiana <alekseevadev@gmail.com>
2023-06-07 10:30:18 -0700

    add placeholder to amount input

--- pages/send.tsx
+++ pages/send.tsx
@@ -1,8 +1,8 @@
 import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
 import Icon from "components/Icon";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import AssetBalance from "components/AssetBalance";
-
+import { FieldValues, useForm } from "react-hook-form";
 
 const Send = () => {
   const [balanceError, setBalanceError] = useState(false);
@@ -12,7 +12,13 @@ const Send = () => {
       <Flex justifyContent="center" mixBlendMode="overlay" mb="70px">
         <Icon name="arrows" size="153px" />
       </Flex>
-      <Flex width="343px" flexDirection="column" alignItems="center" mx="auto" mb="43px">
+      <Flex
+        width="343px"
+        flexDirection="column"
+        alignItems="center"
+        mx="auto"
+        mb="43px"
+      >
         <Input
           placeholder="Recipient"
           mb="24px"
@@ -22,14 +28,22 @@ const Send = () => {
           fontWeight="400"
           _placeholder={{ color: "#155A11", opacity: 1 }}
         />
-        <Input height="64px" bg="rgba(255, 255, 255, 0.8)" mb="6px" />
+        <Input
+          height="64px"
+          bg="rgba(255, 255, 255, 0.8)"
+          mb="8px"
+          fontSize="16px"
+          fontWeight="400"
+          placeholder="$0"
+          _placeholder={{ color: "#33912E", opacity: 1 }}
+        />
         <Flex
           alignSelf="flex-start"
           justifyContent="space-between"
           width="100%"
         >
           <Box fontWeight="400" color="#63676F">
-            <AssetBalance setBalanceError={setBalanceError}/>
+            <AssetBalance setBalanceError={setBalanceError} />
           </Box>
           <Box>
             <Icon name="maticGray" />
@@ -41,12 +55,13 @@ const Send = () => {
         border="2px solid #0D7007"
         borderRadius="40px"
         height="64px"
-        bg="transparent"
+        bg="transparent" // change if enabled
         fontSize="24px"
         fontFamily="satoshi"
         color="#0D7007"
         px="24px"
         py="16px"
+        disabled
       >
         <Text>Send payment</Text>
         <Box ml="auto">
