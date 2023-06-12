59fcdf8dae076b67021d02893201cc66af4a9c83
alekseevaiana <alekseevadev@gmail.com>
2023-06-12 14:00:23 -0700

    cleanup

--- pages/send.tsx
+++ pages/send.tsx
@@ -1,20 +1,8 @@
-import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
-import Icon from "components/Icon";
-import React, { useState } from "react";
-import AssetBalance from "components/AssetBalance";
-import { FieldValues, useForm } from "react-hook-form";
+import { Flex } from "@chakra-ui/react";
+import React from "react";
 import SendScreensCollection from "components/SendScreensCollection";
 
-const Recipient = () => {
-  // second screen
-  // input for recipient
-  // helpers buttons: when discord, validate input to fill discord username, when google, validate input to fill email address
-  // confirm button -> onClick -> save value for input1 on screen 1 => go to screen 1
-};
-
 const Send = () => {
-  const [balanceError, setBalanceError] = useState(false);
-
   return (
     <Flex width="343px" mx="auto">
       <SendScreensCollection />
