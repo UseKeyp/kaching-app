59a61ae1fb083f7eb734b667187df814f2ba0f16
jahabeebs <jahgoreds@gmail.com>
2023-06-15 16:18:30 -0400

    apps page

--- pages/apps.tsx
+++ pages/apps.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Flex, Image, Text } from "@chakra-ui/react";
+import styles from "../components/Play/Play.module.css";
 
 const Apps = () => {
-  return <>Apps</>;
+  return (
+      <>
+        <Flex
+            flexDirection="column"
+            alignItems="start"
+            maxW="full"
+            mx="auto"
+        >
+          <Image
+              height="80px"
+              width="80px"
+              src="/puzzle/puzzle.png"
+              className={styles.puzzleIcon}
+              alt="Puzzle Game"
+          />
+          <Text className={styles.puzzleIconText}>
+            Puzzle
+          </Text>
+        </Flex>
+      </>
+  );
 };
 
 export default Apps;
