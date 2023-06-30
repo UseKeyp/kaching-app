3c6a768537cbe5f0ddd4b9cd4ec0bf8915d4cf26
alekseevaiana <alekseevadev@gmail.com>
2023-06-30 08:31:11 -0700

    assets like btns

--- components/Asset.tsx
+++ components/Asset.tsx
@@ -1,7 +1,38 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { useBalance } from "context/BalanceContext";
 import { useFormContext } from "context/FormContext";
 import React from "react";
+import AssetRow from "./AssetRow";
 import Balances from "./Balances";
+
+interface AssetsBtnsListProps {
+  onClick?: (token: string) => void;
+}
+
+const AssetsBtnsList: React.FC<AssetsBtnsListProps> = ({ onClick }) => {
+  const { balances } = useBalance();
+  const assetsList = balances && Object.values(balances);
+
+  return (
+    <>
+      {assetsList &&
+        assetsList.length > 1 &&
+        assetsList?.map((asset: any) => {
+          return (
+            <Box
+              key={asset.symbol}
+              onClick={() => (onClick ? onClick(asset.symbol) : null)}
+              bg="rgba(255, 255, 255)"
+              borderRadius="40px"
+              boxShadow="0px 1px 0px #C6E3F3"
+            >
+              <AssetRow asset={asset} />
+            </Box>
+          );
+        })}
+    </>
+  );
+};
 
 const Asset = ({ goToStep }: { goToStep?: any }) => {
   const { setAsset } = useFormContext();
@@ -11,6 +42,7 @@ const Asset = ({ goToStep }: { goToStep?: any }) => {
     goToStep(3);
     return;
   };
+  
   return (
     <Box overflow="hidden" width="343px" mx="auto">
       <Heading
@@ -23,14 +55,9 @@ const Asset = ({ goToStep }: { goToStep?: any }) => {
       >
         Asset
       </Heading>
-      <Box
-        bg="rgba(255, 255, 255, 0.8)"
-        borderRadius="8px"
-        width="100%"
-        overflow="hidden"
-      >
-        <Balances onClick={handleClick} />
-      </Box>
+      <Flex width="100%" overflow="hidden" flexDirection="column" gap="24px">
+        <AssetsBtnsList onClick={handleClick}/>
+      </Flex>
     </Box>
   );
 };
