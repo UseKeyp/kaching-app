0ae9cc1c8aa44abf07d71914ad92fc375a1534e7
alekseevaiana <alekseevadev@gmail.com>
2023-07-13 11:26:16 -0700

    fix typescript errors

--- components/NFTsList.tsx
+++ components/NFTsList.tsx
@@ -4,20 +4,32 @@ import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import { KEYP_BASE_URL_V1, supportedAssets } from "utils/general";
 
+interface Media {
+  thumbnail?: string;
+  // Other properties...
+}
+
+interface NFT {
+  media?: Media[];
+  title?: string;
+  description?: string;
+}
+
 const NFTsList = () => {
-  const [nftsList, setNftsList] = useState([]);
+  const [nftsList, setNftsList] = useState<Partial<NFT>[]>([]);
   const { data: session } = useSession();
 
   const fetchNFTs = () => {
-    const ACCESS_TOKEN = session?.user.accessToken;
-    // const userId = "0x3ab208D3CE512F2ac0Aa821Eecf2B816A96799B0" // account with nfts
-    const userId = session?.user.id;
+    const ACCESS_TOKEN = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+    // const ACCESS_TOKEN = session?.user.accessToken;
+    const userId = "0x3ab208D3CE512F2ac0Aa821Eecf2B816A96799B0" // account with nfts
+    // const userId = session?.user.id;
     const options = {
       headers: {
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
     };
-    
+
     // const getNft = `http://api.usekeyp.com/v1/nfts/${userId}`;
     const getNft = `http://localhost:4001/v1/nfts/${userId}`;
 
@@ -41,7 +53,12 @@ const NFTsList = () => {
 
   console.log({ nftsList });
   return (
-    <Box mx="auto" width="343px" fontFamily="satoshi" display={nftsList.length !== 0 ? "block" : "none"}>
+    <Box
+      mx="auto"
+      width="343px"
+      fontFamily="satoshi"
+      display={nftsList.length !== 0 ? "block" : "none"}
+    >
       <Heading
         as="h2"
         fontFamily="satoshi"
@@ -54,7 +71,9 @@ const NFTsList = () => {
       </Heading>
       <Flex flexWrap="wrap" gap="25px">
         {nftsList.map((nft, index) => {
-          return nft.media.length !== 0 && nft.media[0].thumbnail ? (
+          return nft.media &&
+            nft.media.length !== 0 &&
+            nft.media[0].thumbnail ? (
             <Flex
               width="96px"
               height="96px"
